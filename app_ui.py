46f128fade3b7153a27082a0bc1b267dedbdecb3
Dhruv <dhruvgupta0311@gmail.com>
2026-04-06 22:15:43 +0530

    Added UI

---
+++ app_ui.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import joblib 
+st.set_page_config(page_title="Fake News Detector")
+@st.cache_resource
+def load_assets():
+    model = joblib.load('models/logistic_model.joblib')
+    vectorizer = joblib.load('models/tfidf_vectorized.joblib')
+    return model, vectorizer
+m, v = load_assets()
+with st.sidebar:
+    st.title("Stats")
+    st.metric("Dataset size", "72k")
+    st.metric("Test Accuracy", "93%")
+    
+st.title("Fake News Detection")
+user_input = st.text_area("Enter the article text here: ", height=250)
+
+if st.button("Predict"):
+    if user_input:
+        with st.spinner("Predicting..."):
+            user_input_vectorized = v.transform([user_input])
+            prediction = m.predict(user_input_vectorized)[0]
+            probability = m.predict_proba(user_input_vectorized)[0]
+            if prediction == 1:
+                st.error(f"Prediction: Fake News")
+                st.metric(label="Confidence", value=f"{probability[1]*100:.2f}%")
+            else:
+                st.success("Prediction: Real News")
+                st.metric(label="Confidence", value=f"{probability[0]*100:.2f}%")
+    else:
+        st.warning("Enter text to predict.")
+                
+
+    
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pandas as pd
-
 import nltk
 import os
 venv_path = os.path.join(os.getcwd(), '.venv', 'nltk_data')
@@ -8,10 +7,10 @@ from src.data_processor import DataProcessor
 from src.model_trainer import ModelTrainer
 process = DataProcessor()
 
-# print("Processing 72k rows from dataset")
-# df_subset = process.prep_dataframe('WELFake_Dataset.csv', nrows=72000)
-# df_subset.to_csv('final_processed.csv', index=False)
-# print("final Sample of processed data saved to 'final_processed.csv'.")
+print("Processing 72k rows from dataset")
+df_subset = process.prep_dataframe('WELFake_Dataset.csv', nrows=72000)
+df_subset.to_csv('final_processed.csv', index=False)
+print("final Sample of processed data saved to 'final_processed.csv'.")
 trainer = ModelTrainer()
 X_train, X_test, Y_train, Y_test = trainer.prepare_data('final_processed.csv')
 trainer.train_model_logistic(X_train, Y_train)
